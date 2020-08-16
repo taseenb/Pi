f5529eec2f12c74639a5180e0d083d1b621ee659
taseenb <esteban.almiron@gmail.com>
2020-08-16 02:33:16 -0300

    secrets example

---
+++ secrets-example.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+
+  // DB
+  'db.host' => '',
+  'db.name' => '',
+  'db.username' => '',
+  'db.password' => '',
+
+  // Email
+  'email.info' => '',
+  'email.admin' => '',
+  'email.host' => '',
+  'email.username' => '',
+  'email.password' => '',
+
+  // Sandbox
+  'sandbox' => ''
+
+);
